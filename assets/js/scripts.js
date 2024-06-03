efb39997594e37ce5837b4c72906dad9f7a1a431
Pedrozle <pedrozle@outlook.com>
2024-06-02 20:17:30 -0400

    atualizados

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,11 +12,33 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function changeLanguage(lang) {
+  const link = document.getElementById("link_curriculo");
+
+  if (lang === "pt-br") {
+    link.href = "docs/Curriculo - Joao - PT.pdf";
+  } else {
+    link.href = "docs/Curriculo - Joao - en.pdf";
+  }
+
   const elements = document.querySelectorAll("[data-translate]");
   elements.forEach((element) => {
     const key = element.getAttribute("data-translate");
     element.innerHTML = translations[lang][key];
   });
+
+  const elementsArray = Array.from(elements);
+
+  const res_desc = elementsArray.find(
+    (el) => el.getAttribute("data-translate") === "resume_description"
+  );
+  const res_desc_link = elementsArray.find(
+    (el) => el.getAttribute("data-translate") === "resume_description_link"
+  );
+
+  res_desc.innerHTML = translations[lang]["resume_description"];
+  res_desc_link.innerHTML = translations[lang]["resume_description_link"];
+
+  res_desc.appendChild(res_desc_link);
 }
 
 function enviaEmail() {
--- assets/js/translations.js
+++ assets/js/translations.js
@@ -6,7 +6,18 @@ export const translations = {
         "sidenav_projects": "Projetos",
         "sidenav_contact": "Contato",
         "about_title": "Sobre mim",
-        "about_description": "João Pedro, tenho 23 anos e sou estudante de Ciência da Computação pela Fundação Universidade Federal de Rondônia - UNIR! Sou Desenvolvedor Front-End e estudando para ser Desenvolvedor Full Stack, ainda chegarei lá! Apaixonado por jogos e por modelagens, procuro estar antenado em todas as novidades no mundo do desenvolvimento de jogos."
+        "about_description": "João Pedro, tenho 23 anos e sou estudante de Ciência da Computação pela Fundação Universidade Federal de Rondônia - UNIR! Sou Desenvolvedor Front-End e estudando para ser Desenvolvedor Full Stack, ainda chegarei lá! Apaixonado por jogos e por modelagens, procuro estar antenado em todas as novidades no mundo do desenvolvimento de jogos.",
+        "about_profile_title": "Desenvolvedor de sistemas Web &amp; mobile",
+        "about_profile_item_tel": "Telefone",
+        "about_profile_item_location": "Endereço",
+        "about_profile_item_age": "Idade",
+        "about_profile_item_formation":"Formação",
+        "about_profile_motto": "'ser pessimista dá menos trabalho, e também é uma maneira de autopreservação' - José Carlos, 2023",
+        "skills_title": "Tecnologias",
+        "skills_description": "Tecnologias que eu possuo conhecimento",
+        "resume_title": "Currículo",
+        "resume_description": "Um breve resumo e algumas informações importantes do meu currículo! Você também poderá fazer o download ",
+        "resume_description_link": "clicando aqui!",
     },
     "en": {
         "sidenav_home": "Home",
@@ -15,6 +26,17 @@ export const translations = {
         "sidenav_projects": "Projects",
         "sidenav_contact": "Contact",
         "about_title": "About me",
-        "about_description": "João Pedro, I am 23 years old and I am a student of Computer Science from the Federal University of Rondônia Foundation - Unir! I'm a front-end developer and studying to be a full stack developer, I'll get there still! Passionate about games and modeling, I try to be tuned in all the news in the world of game development."
+        "about_description": "João Pedro, I am 23 years old and I am a student of Computer Science from the Federal University of Rondônia Foundation - Unir! I'm a front-end developer and studying to be a full stack developer, I'll get there still! Passionate about games and modeling, I try to be tuned in all the news in the world of game development.",
+        "about_profile_title": "Web &amp; mobile Developer",
+        "about_profile_item_tel": "Phone",
+        "about_profile_item_location": "Address",
+        "about_profile_item_age": "Age",
+        "about_profile_item_formation":"Formation",
+        "about_profile_motto": "'Being pessimistic is less work, and it is also a way of self-preservation' - José Carlos, 2023",
+        "skills_title": "Tecnologies",
+        "skills_description": "Tecnologies I have knowledge of",
+        "resume_title": "Resume",
+        "resume_description": "A brief resume and some important information. You can download it ",
+        "resume_description_link": "by clicking here!",
     }
 }
