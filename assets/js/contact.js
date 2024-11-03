6d9b6d3c3e84f6b2f5b408fb1c8d26c27abc096d
Pedrozle <pedrozle@outlook.com>
2024-11-03 10:15:50 -0400

    refac. [translation]: configurado tradução para pagina contact

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -19,3 +19,50 @@ document
 
     window.location.href = mailtoLink;
   });
+
+var lang = "pt";
+window.onload = function () {
+  lang = window.localStorage.getItem("language") || "pt";
+  changeLanguage();
+};
+
+function changeLanguage() {
+  const elements = document.querySelectorAll("[data-translate]");
+  elements.forEach((element) => {
+    const key = element.getAttribute("data-translate");
+    element.innerHTML = translations[lang][key];
+  });
+
+  var inputs = document.querySelectorAll("input");
+
+  inputs.forEach((input) => {
+    input.setAttribute("placeholder", translations[lang][`input-${input.name}`]);
+  });
+
+  document.querySelector("textarea").setAttribute("placeholder", translations[lang]['textarea'])
+  
+
+}
+
+const translations = {
+  pt: {
+    "nav-link-back": "Voltar",
+    "contact-header": "CONTATO",
+    "contact-motto": "Tem alguma vaga? Me envie um email que eu te respondo o mais breve possível",
+    "input-name": "Digite seu nome",
+    "input-email": "Digite seu email",
+    "input-phone": "Número de celular",
+    "textarea": "Escreva aqui a sua mensagem",
+    "form-button": "Entrar em contato",
+  },
+  en: {
+    "nav-link-back": "Go Back",
+    "contact-header": "CONTACT",
+    "contact-motto": "Do you have any positions? Send me an email and I will respond as soon as possible.",
+    "input-name": "Enter your name",
+    "input-email": "Enter your email",
+    "input-phone": "Enter your phone number",
+    "textarea": "Write here your message",
+    "form-button": "Contact",
+  },
+};
