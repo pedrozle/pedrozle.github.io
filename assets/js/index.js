622e9fca46805ac049551c9f293c955e61c63ad8
Pedrozle <pedrozle@outlook.com>
2024-11-03 09:50:55 -0400

    refac. [translation]: configurado tradução para pagina about me

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,31 +11,12 @@ function changeLanguage() {
   const selectElement = document.getElementById("options");
   const lang = selectElement.value;
   window.localStorage.setItem("language", lang);
-  const link = document.getElementById("link_curriculo");
-  //   if (lang === "pt-br") {
-  //     link.href = "docs/Curriculo - Joao - PT.pdf";
-  //   } else {
-  //     link.href = "docs/Curriculo - Joao - en.pdf";
-  //   }
 
   const elements = document.querySelectorAll("[data-translate]");
   elements.forEach((element) => {
     const key = element.getAttribute("data-translate");
     element.innerHTML = translations[lang][key];
   });
-
-  const elementsArray = Array.from(elements);
-  const res_desc = elementsArray.find(
-    (el) => el.getAttribute("data-translate") === "resume_description"
-  );
-
-  const res_desc_link = elementsArray.find(
-    (el) => el.getAttribute("data-translate") === "resume_description_link"
-  );
-
-  res_desc.innerHTML = translations[lang]["resume_description"];
-  res_desc_link.innerHTML = translations[lang]["resume_description_link"];
-  res_desc.appendChild(res_desc_link);
 }
 
 const translations = {
