0f8b8595e290b46b989072f7aeb48a7f176d8960
Pedrozle <pedrozle@outlook.com>
2025-05-05 21:32:23 -0400

    wip

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+const cover = document.getElementById("cover");
+const date = document.getElementById("date");
+const time = document.getElementById("time");
+
+const now = new Date();
+
+date.innerHTML = now.toLocaleDateString("pt-BR", {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+});
+
+// Hora: apenas hora e minuto (ex: "20:23")
+time.innerHTML = now.toLocaleTimeString("pt-BR", {
+  hour: "2-digit",
+  minute: "2-digit",
+});
+
+cover.addEventListener("click", () => {
+  cover.style.transform = "translateY(-100%)";
+
+  cover.addEventListener(
+    "transitionend",
+    () => {
+      cover.style.display = "none"; // Remove a camada do cover após o efeito
+    },
+    { once: true }
+  );
+});
