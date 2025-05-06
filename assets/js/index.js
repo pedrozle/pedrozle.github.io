5ff8b708d0d47f27d97fc08c6f877d78f2dc319b
Pedrozle <pedrozle@outlook.com>
2025-05-06 11:07:38 -0400

    update portifólio

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 const cover = document.getElementById("cover");
 const date = document.getElementById("date");
+const dater = document.getElementById("dater");
 const time = document.getElementById("time");
+const timer = document.getElementById("timer");
 
 const now = new Date();
 
@@ -13,6 +15,17 @@ date.innerHTML = now.toLocaleDateString("pt-BR", {
 
 // Hora: apenas hora e minuto (ex: "20:23")
 time.innerHTML = now.toLocaleTimeString("pt-BR", {
+  hour: "2-digit",
+  minute: "2-digit",
+});
+
+dater.innerHTML = now.toLocaleDateString("pt-BR", {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+});
+
+timer.innerHTML = now.toLocaleTimeString("pt-BR", {
   hour: "2-digit",
   minute: "2-digit",
 });
